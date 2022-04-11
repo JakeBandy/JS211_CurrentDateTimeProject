ce93df52992dfc12b5131196165da233fbf834c3
JakeBandy <jakebandy13@hotmail.com>
2022-04-11 18:03:50 -0500

    finish

--- main.js
+++ main.js
@@ -15,10 +15,24 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
+const convertNumberToString = (str) => {
+  const result = Number(str)
+console.log(typeof result);
+return result;
+}
 
+console.log(convertNumberToString("5"))
 
 
 // Write a JavaScript program to convert a string to the number.
+const convertStringToNumber = (num) => {
+  const result = String(num)
+console.log(typeof result);
+return result;
+}
+
+console.log(convertStringToNumber(5))
+
 
 
 
@@ -30,21 +44,43 @@ const displayDate = () => {
   // * NaN
   // * String
   
+  function checkDataType(element) {
+    return typeof element
+  }
 
-  
+  console.log(checkDataType())
 // Write a JavaScript program that adds 2 numbers together.
-
+let total = 2 + 2;
 
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+function trueTwo(a, b, c){
+  if(a === b && c > b){
+    return true;
+  }
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+const x = 4;
+const y = 4;
+const z = 7;
+
+function oneTrue(x, y, z){
+  if(x === y || x === z){
+    return true;
+  } 
+}
 
 
 // Write a JavaScript program that runs when both things are not true.  
+function noneTrue(a, b, c){
+  if(a != b && a != c){
+    return true;
+  }
+}
 
 // ***************************
 //         PART TWO
@@ -58,7 +94,17 @@ const displayDate = () => {
 // 6. go to `index.html` 
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
+document.getElementById("num2str")
+const results = convertNumberToString(num2str.value)
 
+num2str.value
+console.log(results)
+
+document.getElementById("str2num")
+const results = convertStringToNumber(str2num.value)
+
+str2num.value
+console.log(results)
 
 
 // Additional Resources
